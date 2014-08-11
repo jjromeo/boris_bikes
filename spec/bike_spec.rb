697c1494cb0bc5c364ad5619af887d3e822778cb
Jerome Pratt <jerome.pratt@outlook.com>
2014-08-11 18:28:04 +0100

    adding ability for bike to get fixed

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -6,15 +6,22 @@ describe Bike do
 	# this is a specific feature (behaviour)
 	# that we expect to be present
 	it "should not be broken after we create it" do 
-		the_bike = Bike.new # initialise a new object of Bike class
+		bike = Bike.new # initialise a new object of Bike class
 		# expect an instance of this class to have 
 		# a method "broken?" that should return false"
-		expect(the_bike).not_to be_broken
+		expect(bike).not_to be_broken
 	end
 
 	it "should be able to break" do
 		bike = Bike.new
 		bike.break!
 		expect(bike).to be_broken
+	end
+
+	it "should be able to get fixed" do 
+		bike = Bike.new
+		bike.break!
+		bike.fix!
+		expect(bike).not_to be_broken
 	end
 end
