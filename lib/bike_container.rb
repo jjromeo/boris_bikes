e922ac031aaf4615b22aa8e7403938da2bd081cf
Jerome Pratt <jerome.pratt@outlook.com>
2014-08-12 17:02:37 +0100

    Exercise 1- made the release() method not allow bikes which aren't there

---
+++ lib/bike_container.rb
@@ -0,0 +1,39 @@
+module BikeContainer
+		DEFAULT_CAPACITY = 10
+
+	def bikes
+		@bikes ||= []
+	end
+
+	def capacity
+		@capacity ||= DEFAULT_CAPACITY
+	end
+
+	def capacity=(value)
+		@capacity = value
+	end
+
+	def bike_count()
+		bikes.count
+	end
+
+	def dock(bike)
+		raise "Holder is full" if full?
+		bikes << bike
+	end
+
+	def release(bike)
+		raise "No bikes available" if bike_count == 0
+		bikes.delete(bike)
+	end
+
+	def full?
+		bike_count == capacity
+	end
+
+	def available_bikes
+		bikes.reject {|bike| bike.broken? }
+	end
+
+
+end
